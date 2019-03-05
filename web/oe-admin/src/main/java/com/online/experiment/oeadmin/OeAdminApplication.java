e784ddb02bd0ad0d7b543ea8ad0b5d73718dc3c2
wangwenjie <2285262007@qq.com>
2019-03-05 14:30:06 +0800

    创建oe-admin模块

---
+++ web/oe-admin/src/main/java/com/online/experiment/oeadmin/OeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.online.experiment.oeadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OeAdminApplication.class, args);
+    }
+
+}
